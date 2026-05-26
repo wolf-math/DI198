6e94f0b20dd925302967afa0bc06ffe2aae0816b
Aaron Wolf <aaron.wolf@gmail.com>
2026-05-26 16:27:50 +0300

    birthday update

--- week2/day3/birthday.py
+++ week2/day3/birthday.py
@@ -2,22 +2,36 @@
 
 # For example : "My birthday is in 45 days, and 10:29:46"
 
-from datetime import datetime, timedelta
+from datetime import datetime
 
-my_birthday = (19, 6)
-now = datetime.now()
+def parse_time(time_obj):
+    days = time_obj.days
+    hours, remainder = divmod(time_obj.seconds, 3600)
+    minutes, seconds = divmod(remainder, 60)
 
-next_birthday_year = now.year
-if now.month > my_birthday[1] and now.day > my_birthday[0]:
-    next_birthday_year += 1
+    return (days, hours, minutes, seconds)
 
-next_birthday_date = datetime.strptime(f"{my_birthday[0]}/{my_birthday[1]}/{next_birthday_year}", "%d/%m/%Y")
 
-months_to_bday = next_birthday_date.month - now.month
-days_to_bday = next_birthday_date.day - now.day
-hours_to_bday = next_birthday_date.hour - now.hour
-minutes_to_bday = next_birthday_date.minute - now.minute
+def birthday_countdown(day, month):
+    now = datetime.now()
 
-time_to_bday = next_birthday_date - now
+    next_birthday_year = now.year
+    if now.month > month and now.day > day:
+        next_birthday_year += 1
 
-print(f"My birthday is in {time_to_bday.days} days {time_to_bday.seconds // 60} minutes.")
+    next_birthday_date = datetime.strptime(f"{day}/{month}/{next_birthday_year}", "%d/%m/%Y")
+
+    return next_birthday_date - now
+
+
+def main():
+    user_month = int(input("What is your birthday month? (number) "))
+    user_day = int(input("What day of the month is your birthday? (number) "))
+
+    countdown = birthday_countdown(user_day, user_month)
+
+    days, hours, minutes, seconds = parse_time(countdown)
+
+    print(f"Birthday is in {days} days, {hours} hours, {minutes} minutes, and {seconds} seconds.")
+
+main()
